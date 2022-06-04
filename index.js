9f4dad470d4b04d394f6d765d4d2d98218a5d7de
--replace-all <terrencejung928@gmail.com>
2022-06-03 17:54:18 -0700

    commit tree 196e13f0cdb1784484679e9d6d2e6e6d3366d97a

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n^2) => We have a nested loop with constant time operations inside of the inner loop.
 */
 
 /* 
   Add your pseudocode here
-*/
+  For each number i in the array
+    For each number j in the array 
+      If the sum of i and j equals target, we return true
+*/    
 
 /*
   Add written explanation of your solution here
+  Perform a nested loop in which we observe the sum of every pair of 
+  numbers and return true if it equals target
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,7 +33,8 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
-  console.log("");
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([3, 8, 12, 4, 25, 7], 25));
 
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
